b651adce1aa5ee9e49a0fbdb2899ca0fad348bfb
Ian Steele <isteele@umich.edu>
2025-03-26 13:42:51 -0400

    Attacker V2 Update

--- attacker_v2/attacker_v2.ino
+++ attacker_v2/attacker_v2.ino
@@ -2,41 +2,29 @@
 const int trigPin = 9;
 const int echoPin = 10;
 
-int pulseDelay = 5; // ms
-int count = floor(60/(pulseDelay/1000));  // 60sec of operation
+const int period = 200; // Total cycle time in milliseconds
+const float pi = 3.14159;
 
 void setup() {
   // Initialize serial communication
   Serial.begin(9600);
-  Serial.println("Hello");
-  // randomSeed(analogRead(0));
-  
-  // Set the trigPin as an OUTPUT
   pinMode(trigPin, OUTPUT);
-  
-  // Set the echoPin as an INPUT
   pinMode(echoPin, INPUT);
-  Serial.println(count);
 }
 
 void loop() {
-  while (count > 0){
-      // Sends a pulse of HIGH for 10 microseconds on trigPin
-      digitalWrite(trigPin, LOW);
-      int rand1 = random(50, 10000);
-      delayMicroseconds(rand1);
-      digitalWrite(trigPin, HIGH);
-      int rand2 = random(50, 10000);
-      delayMicroseconds(rand2);
-      digitalWrite(trigPin, LOW);
+  static int timeStep = 0;
+  float normalizedTime = (float)timeStep / period; // Normalize to range [0,1]
+  int delayTime = 2 + (998 * (sin(2 * pi * normalizedTime) + 1) / 2); // Map sine wave to [2, 1000]us
 
-      Serial.print(0); // To freeze the lower limit
-      Serial.print(" ");
-      Serial.print(10000); // To freeze the upper limit
-      Serial.print(" ");
-      Serial.print(rand1);
-      Serial.print(" ");
-      Serial.println(rand2);
-      count--;
-  }
+  // Send 10-microsecond trigger pulse
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Wait before next pulse
+  delayMicroseconds(delayTime);
+
+  // Increment timeStep and wrap around
+  timeStep = (timeStep + 1) % period;
 }
