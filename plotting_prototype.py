e9a26211e256487f2f501f8f3bfb37ec5c5f739e
Ian Steele <isteele@umich.edu>
2025-04-13 13:36:02 -0400

    STFT changes and cleanup in plotting_prototype

--- plotting_prototype.py
+++ plotting_prototype.py
@@ -11,7 +11,7 @@ import time
 
 # Settings
 port_name = "/dev/tty.usbmodem1103"  # Must be changed if port changes
-plotting_rate = 100 # in ms
+plotting_rate = 50 # in ms
 list_length = int(10000 / plotting_rate)  # 10 seconds of data
 
 #TODO: Holt EMA settings
@@ -19,9 +19,9 @@ tau_l = 0.1
 tau_m = 0.5
 
 #TODO: STFT settings
-N = 64
-L = 2
-spec_length = int((list_length - N)/L)
+N = list_length//5
+L = list_length//20
+spec_length = (list_length - N)//L
 
 # 400 cm/s for top speed of hand
 #TODO: Jamming detection settings
@@ -31,7 +31,7 @@ window_size = int(2000 / plotting_rate)  # full window (~2000ms)
 drop_window = int(500 / plotting_rate)   # how many samples ago to compare against (~500ms)
 
 #TODO: Toggle if sensor is connected for proper debugging away from an STM/Arduino
-sensor_connected = True                # Set to True if sensor is sending raw data to USB
+sensor_connected = False                # Set to True if sensor is sending raw data to USB
 
 # Initialize flags
 jamming = False
@@ -40,9 +40,9 @@ spoofing = False
 # Initialize arrays and serial communication
 if sensor_connected:
     ser = serial.Serial(port_name, 115200, timeout=1)
-data = [1.0E-6 for i in range(list_length)]
-raw_data = [1.0E-6 for i in range(list_length)]
-processed_data = [1.0E-6 for i in range(list_length)]
+data = [1.0E-10 for i in range(list_length)]
+raw_data = [1.0E-10 for i in range(list_length)]
+processed_data = [1.0E-10 for i in range(list_length)]
 timestamps = np.linspace(-10, 0, list_length).tolist()
 
 # Initialize processing classes
@@ -77,8 +77,8 @@ def update(frame):
             processed_data.append(processed_value)
 
             # Check for jamming
-            jam_detect, maybe_jam_detect = jamming_detector.update(raw_value)
-            spoof_detect = False    # TODO
+            #jam_detect, maybe_jam_detect = jamming_detector.update(raw_value)
+            #spoof_detect = False
 
             # Update the data arrays
             raw_data.pop(0)
@@ -88,42 +88,31 @@ def update(frame):
             ax1.clear()
             ax1.plot(timestamps, raw_data, label = "Raw Data")
             ax1.plot(timestamps, processed_data, label = "Processed Data")
-            ax1.set_ylim(0, 800)
+            ax1.set_ylim(0, 300)
             ax1.set_xlim(-10, 0)
             ax1.legend(loc="upper left")
             ax1.set_xlabel("Time (seconds)")
             ax1.set_ylabel("Distance (cm)")
 
-            
-            if jam_detect:
-                ax1.title.set_text("Attack Type: Jamming")
-            elif spoof_detect:
-                ax1.title.set_text("Attack Type: Spoofing")
-            else:
-                if maybe_jam_detect:
-                    ax1.title.set_text("Possible Jamming Detected")
-                else:
-                    ax1.title.set_text("No Jamming Detected")
-            
             fs = 1/(plotting_rate/1000)
-            spectrogram_rect = np.zeros((int(N/2), spec_length))
-            for i in range(0, spec_length):
-                data_window = raw_data[i*L:i*L+N]
-                win = np.fft.fft(data_window, N)
-                temp_mag = np.abs(win[:len(win)//2])
-                temp_mag[temp_mag == 0] = 1e-10
-                spectrogram_rect[:, i] = 20*np.log10(temp_mag)  # Spectrogram data
+            window = np.ones(N)
+            X = stft(raw_data, window, L)
+            t = np.linspace(-10*(list_length - N)/list_length, 0, (list_length - N)//L)
+            f = np.linspace(0, fs/2, N//2)
+
             open("output.txt", "w").close()
             with open("output.txt", "a") as f:
-                    line = ' '.join(str(x) for x in spectrogram_rect[:, spec_length-1])
+                    line = ' '.join(str(x) for x in X[:, spec_length-1])
                     f.write(line + '\n')
-            time_ms = np.arange(spec_length) * (L / fs)
-            freq_kHz = np.linspace(0, fs/2, int(N/2))
+
             ax2.clear()
-            ax2.pcolormesh(time_ms, freq_kHz, spectrogram_rect, shading='gouraud', cmap='inferno', vmin=0, vmax=70)
+            ax2.pcolormesh(t, f, X, shading='gouraud', cmap='inferno', vmin=1, vmax=70)
             ax2.title.set_text("Log-Amplitude Spectrogram")
             ax2.set_xlabel("Time")
-            ax2.set_ylabel("Frequency")
+            ax2.set_ylabel("Frequency (Hz)")
+
+            detect = detect_high_freq(raw_data[list_length - list_length // 4:], fs, 40, 10)
+
         except ValueError:
             print("Error: Incorrect type. Please enter a valid string of numbers with comma separation.")
 
@@ -149,6 +138,17 @@ def detect_high_freq(window, fs, threshold_freq, some_threshold):
     # Detect power in high-frequency bins
     high_freq_power = np.sum(spectrum[freq > threshold_freq])
     return high_freq_power > some_threshold
+
+def stft(x, window, stride):
+    time_length = (len(x) - len(window))//stride
+    X = np.zeros((len(window)//2, time_length), dtype=np.float64)
+    for i in range(0, time_length):
+        data = x[i*stride:i*stride+len(window)]
+        freq_data = np.abs(np.fft.fft(data * window, len(window)))
+        freq_data[freq_data == 0] = 1e-10
+        freq_data = freq_data[:len(freq_data)//2]
+        X[:, i] = 20*np.log10(freq_data)
+    return X
 
 # Main loop
 fig, (ax1, ax2) = plt.subplots(1, 2)
